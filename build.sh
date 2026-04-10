d4ecaf7643b4cdb7025c5e551a93d6811c42211d
VS-123 <>
2026-04-10 04:08:27 +0000

    browser: revert to firefox

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-	echo "Usage: $0 <architecture> (e.g., x86_64 or aarch64)"
+	echo "[USAGE] $0 <ARCHITECTURE> (E.G., X86_64 OR AARCH64)"
 	exit 1
 fi
 
@@ -22,51 +22,53 @@ rm ./data/splash.png &&
 	cp ../kalium-iso-overlay/etc/issue ./data/issue
 
 yes | sudo ./mklive.sh \
+	-I ../kalium-iso-overlay \
+	-S "acpid dhcpcd sshd"  \
+	-T "Kalium Void" \
 	-a $ARCH \
+	-e /bin/zsh \
+	-o "../kalium-void-$ARCH.iso" \
 	-p "$(cat ../packages.txt)"  \
 	-r ./kalium-repo/hostdir/binpkgs \
-	-S "acpid dhcpcd sshd"  \
-	-I ../kalium-iso-overlay \
-	-x ../finish_kalium_void.sh  \
-	-T "Kalium Void" \
-	-e /bin/zsh \
-	-r "https://github.com/index-0/librewolf-void/releases/latest/download/" \
-	-o "../kalium-void-$ARCH.iso"
+	-x ../finish_kalium_void.sh  
 
-#   Usage: mklive.sh [options]
-#   
-#   Generates a basic live ISO image of Void Linux. This ISO image can be written
-#   to a CD/DVD-ROM or any USB stick.
-#   
-#   To generate a more complete live ISO image, use mkiso.sh.
-#   
+
+
+#-r "https://github.com/index-0/librewolf-void/releases/latest/download/" \
+
+
+
+
+
+
+
 #   OPTIONS
-#    -a <arch>          Set XBPS_ARCH in the ISO image
-#    -b <system-pkg>    Set an alternative base package (default: base-system)
-#    -r <repo>          Use this XBPS repository. May be specified multiple times
-#    -c <cachedir>      Use this XBPS cache directory (default: ./xbps-cachedir-<arch>)
-#    -H <host_cachedir> Use this Host XBPS cache directory (default: ./xbps-cachedir-<host_arch>)
-#    -k <keymap>        Default keymap to use (default: us)
-#    -l <locale>        Default locale to use (default: en_US.UTF-8)
-#    -i <lz4|gzip|bzip2|xz>
-#                       Compression type for the initramfs image (default: xz)
-#    -s <gzip|lzo|xz>   Compression type for the squashfs image (default: xz)
-#    -o <file>          Output file name for the ISO image (default: automatic)
-#    -p "<pkg> ..."     Install additional packages in the ISO image
-#    -g "<pkg> ..."     Ignore packages when building the ISO image
-#    -I <includedir>    Include directory structure under given path in the ROOTFS
-#    -S "<service> ..." Enable services in the ISO image
-#    -e <shell>         Default shell of the root user (must be absolute path).
-#                       Set the live.shell kernel argument to change the default shell of anon.
-#    -C "<arg> ..."     Add additional kernel command line arguments
+#    -a <arch>          SET XBPS_ARCH IN THE ISO IMAGE
+#    -b <system-pkg>    SET AN ALTERNATIVE BASE PACKAGE (DEFAULT: BASE-SYSTEM)
+#    -r <repo>          USE THIS XBPS REPOSITORY. MAY BE SPECIFIED MULTIPLE TIMES
+#    -c <cachedir>      USE THIS XBPS CACHE DIRECTORY (DEFAULT: ./XBPS-CACHEDIR-<ARCH>)
+#    -H <host_cachedir> USE THIS HOST XBPS CACHE DIRECTORY (DEFAULT: ./XBPS-CACHEDIR-<HOST_ARCH>)
+#    -k <keymap>        DEFAULT KEYMAP TO USE (DEFAULT: US)
+#    -l <locale>        DEFAULT LOCALE TO USE (DEFAULT: EN_US.UTF-8)
+#    -i <lz4|gzip|bzip2|XZ>
+#                       COMPRESSION TYPE FOR THE INITRAMFS IMAGE (DEFAULT: XZ)
+#    -s <gzip|lzo|xz>   COMPRESSION TYPE FOR THE SQUASHFS IMAGE (DEFAULT: XZ)
+#    -o <file>          OUTPUT FILE NAME FOR THE ISO IMAGE (DEFAULT: AUTOMATIC)
+#    -p "<pkg> ..."     INSTALL ADDITIONAL PACKAGES IN THE ISO IMAGE
+#    -g "<pkg> ..."     IGNORE PACKAGES WHEN BUILDING THE ISO IMAGE
+#    -I <includedir>    INCLUDE DIRECTORY STRUCTURE UNDER GIVEN PATH IN THE ROOTFS
+#    -S "<service> ..." ENABLE SERVICES IN THE ISO IMAGE
+#    -e <shell>         DEFAULT SHELL OF THE ROOT USER (MUST BE ABSOLUTE PATH).
+#                       SET THE LIVE.SHELL KERNEL ARGUMENT TO CHANGE THE DEFAULT SHELL OF ANON.
+#    -C "<arg> ..."     ADD ADDITIONAL KERNEL COMMAND LINE ARGUMENTS
 #    -P "<platform> ..."
-#                       Platforms to enable for aarch64 EFI ISO images (available: pinebookpro, x13s)
-#    -T <title>         Modify the bootloader title (default: Void Linux)
-#    -v linux<version>  Install a custom Linux version on ISO image (default: linux metapackage).
-#                       Also accepts linux metapackages (linux-mainline, linux-lts).
-#    -x <script>        Path to a postsetup script to run before generating the initramfs
-#                               (receives the path to the ROOTFS as an argument)
-#    -K                 Do not remove builddir
-#    -h                 Show this help and exit
-#    -V                 Show version and exit
+#                       PLATFORMS TO ENABLE FOR AARCH64 EFI ISO IMAGES (AVAILABLE: PINEBOOKPRO, X13S)
+#    -T <title>         MODIFY THE BOOTLOADER TITLE (DEFAULT: VOID LINUX)
+#    -v linux<version>  INSTALL A CUSTOM LINUX VERSION ON ISO IMAGE (DEFAULT: LINUX METAPACKAGE).
+#                       ALSO ACCEPTS LINUX METAPACKAGES (LINUX-MAINLINE, LINUX-LTS).
+#    -x <script>        PATH TO A POSTSETUP SCRIPT TO RUN BEFORE GENERATING THE INITRAMFS
+#                               (RECEIVES THE PATH TO THE ROOTFS AS AN ARGUMENT)
+#    -K                 DO NOT REMOVE BUILDDIR
+#    -h                 SHOW THIS HELP AND EXIT
+#    -V                 SHOW VERSION AND EXIT
 
